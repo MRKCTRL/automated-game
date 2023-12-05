7d5c3414b6603a6a3ed700a87519d389e0329a72
MRKCTRL <davidmbundu@gmail.com>
2023-12-05 13:40:43 +0200

    time.sleep, pillow and pyautogui

---
+++ main.py
@@ -0,0 +1,47 @@
+# to detect the image or the black and white pixels
+from Pil import Image
+# i used py auto to do the aumoation
+import pyautogui
+# i used time to sleep to get to the browser
+import time
+imprt numpy as np
+
+
+# screenWidth()
+time.sleep(5)
+while true:
+    screen = im.getpicel((214,184))
+
+    x1, y11a1 b = 214,250, 60,40
+    combine = (x1+y1+x1+a+y1+b)
+    img = ImageGrab.grab(box)
+    gray = ImageOps.grayscale(image)
+    a = array(gray.get_colors())
+    value = a.sum()
+    if (value != 2293): pytautogui.press('up')
+    # x2=im.getpicel((214,184))
+    # x3=im.getpicel((214,184))
+    # x4=im.getpicel((214,184))
+    # x5=im.getpicel((214,184))
+    #
+    # y1=im.getpicel((214,184))
+    # y2=im.getpicel((214,184))
+    # y3=im.getpicel((214,184))
+    # y4=im.getpicel((214,184))
+    if keyboard.is_pressed('j'):
+        break
+
+# im = Image.open("")
+#
+# time.sleep(4)
+#
+# time.sleep(3)
+#
+# for i in range(20):
+#     pyautogui.press('space')
+#     time.sleep(0.5)
+#
+#
+# dark mode
+# pixels of the white and black
+# i
